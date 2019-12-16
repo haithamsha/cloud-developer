5c9d5d09134a515ecbee0ac5bd771046f4266c51
haithamsha <haithamshaabann@gmail.com>
2019-12-16 23:58:31 +0200

    ignore config and modify some configuration data

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "postgres",
+    "password": "asd123",
+    "database": "carsdb",
+    "host": "localhost",
+    "dialect": "postgres"
+  },
+  "prod": {
+    "username": "devdbuser",
+    "password": "devdbpass",
+    "database": "devdb",
+    "host": "devdb.cywj6c7gn6vx.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+    "aws_media_bucket": "devbucketdev"
   }
 }
