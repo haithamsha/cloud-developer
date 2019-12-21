11aab4c99248307ce50c8dc7706a0911f85eda3e
haithamsha <haithamshaabann@gmail.com>
2019-12-21 23:49:02 +0200

    deploy to the live aws server

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
   "dev": {
-    "username": "postgres",
-    "password": "asd123",
-    "database": "carsdb",
-    "host": "localhost",
+    "username": process.env.POSTGRESS_USERNAME_DEV,
+    "password": process.env.POSTGRESS_PASSWORD_DEV,
+    "database": process.env.POSTGRESS_DATABASE_DEV,
+    "host": process.env.POSTGRESS_DEVHOST_DEV,
     "dialect": "postgres"
   },
   "prod": {
-    "username": "devdbuser",
-    "password": "devdbpass",
-    "database": "devdb",
-    "host": "devdb.cywj6c7gn6vx.us-east-2.rds.amazonaws.com",
+    "username": process.env.POSTGRESS_USERNAME_PROD,
+    "password": process.env.POSTGRESS_PASSWORD_PROD,
+    "database": process.env.POSTGRESS_DATABASE_PROD,
+    "host": process.env.POSTGRESS_HOST_PROD,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "devbucketdev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   }
 }
