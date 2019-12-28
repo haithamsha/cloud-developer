bcb7c05ae583dfe056457623d1a88070f25a60bb
haithamsha <haithamshaabann@gmail.com>
2019-12-28 12:36:56 +0200

    add secret key for jwt

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -15,5 +15,8 @@ export const config = {
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+  },
+  "jwt": {
+    "secret": "hl"
   }
 }
