7f87ca5efea4e1a9d339a0364d06cac1ab2b500f
haithamsha <haithamshaabann@gmail.com>
2020-01-12 16:49:49 +0200

    fixing

--- course-02/exercises/udacity-c2-restapi/udacity-c2-restapi-feed/src/sequelize.ts
+++ course-02/exercises/udacity-c2-restapi/udacity-c2-restapi-feed/src/sequelize.ts
@@ -2,14 +2,14 @@ import {Sequelize} from 'sequelize-typescript';
 import { config } from './config/config';
 
 
-const c = config.dev;
+const c = config.prod;
 
 // Instantiate new Sequelize instance!
 export const sequelize = new Sequelize({
   "username": c.username,
   "password": c.password,
   "database": c.database,
-  "host":     c.host,
+  "host":     "shadevdb.cywj6c7gn6vx.us-east-2.rds.amazonaws.com",
 
   dialect: 'postgres',
   storage: ':memory:',
